e3707131c9253e8a1ecfc1013e3264733d179d8f
armaan <armaan_priyadarshan@outlook.com>
2023-11-20 08:30:13 -0500

    Changed pages to home

--- EZAPCompanion/urls.py
+++ EZAPCompanion/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pages/', include('pages.urls')),
+    path('home/', include('home.urls')),
 ]
