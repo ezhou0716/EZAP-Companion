2aa6e7ce8b542509c1745b7dfcc8d83e34ab5c18
armaan <armaan_priyadarshan@outlook.com>
2023-11-20 08:17:18 -0500

    Small changes

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import home
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", home),
+    path('', views.home, name='home'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
