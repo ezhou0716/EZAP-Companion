ad49bf33cd0a0f21b208a90c5fa8383fcfc76f0b
armaan <armaan_priyadarshan@outlook.com>
2023-11-21 11:48:02 -0500

    Fixed stuff

--- problems/views.py
+++ problems/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def problems(request):
-    return render(request, 'problems.html')
+    template = loader.get_template('problems.html')
+    return HttpResponse(template.render())
