77e104aa7b1d95f80e114fd5ce4a57ae60dde2b7
armaan <armaan_priyadarshan@outlook.com>
2024-01-29 12:13:38 -0500

    doogie

--- problems/views.py
+++ problems/views.py
@@ -1,7 +1,16 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
+from firebase_admin import db
 
 # Create your views here.
+def get_problem():
+    ref = db.reference('/derivatives')
+
+    problems = ref.get()
+
+    return problems
+
+
 def problems(request):
-    template = loader.get_template('problems.html')
-    return HttpResponse(template.render())
+    problem = get_problem()
+    
+    return render(request, 'problems.html')
