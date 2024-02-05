79a161a4331afe15f14a8982ca9c9890f88bb7e8
armaan <armaan_priyadarshan@outlook.com>
2024-02-05 11:20:42 -0500

    doogie

--- problems/views.py
+++ problems/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from firebase_admin import firestore
+import random
 
 # Create your views here.
 def get_problems():
     db = firestore.client() 
     ref = db.collection('derivatives')
 
-    problems = ref.get()
+    problems = [doc.to_dict() for doc in ref.stream()]
     
     return problems
 
 
 def problems(request):
-    problems = get_problems()
-    
-    return render(request, 'problems.html')
+    if request.method == 'POST' and 'new_problem' in request.POST:
+        problems = get_problems()
+        random_problem = random.choice(problems)
+        return render(request, 'problems.html', {'random_problem': random_problem})
+    else:
+        return render(request, 'problems.html')
