47346024733bc47de03f745a0227f1a3876e00a3
armaan <armaan_priyadarshan@outlook.com>
2024-02-26 09:07:05 -0500

    tag retrieval based on unit

--- problems/views.py
+++ problems/views.py
@@ -14,8 +14,19 @@ def get_problems():
 
 def problems(request):
     if request.method == 'POST' and 'new_problem' in request.POST:
-        problems = get_problems()
-        random_problem = random.choice(problems)
-        return render(request, 'problems.html', {'random_problem': random_problem})
+        # selected_unit = request.POST.get('units')
+
+        db = firestore.client() 
+        ref = db.collection('derivatives')
+
+        problems = [doc.to_dict() for doc in ref.stream()]
+
+        random_problem = random.choice(problems) if problems else None
+
+        tags = set()
+        for problem in problems:
+            tags.update(problem['tags'])
+        
+        return render(request, 'problems.html', {'random_problem': random_problem, 'tags': tags})
     else:
         return render(request, 'problems.html')
